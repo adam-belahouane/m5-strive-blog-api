8f85f11bae0dd356a013d289fcb7854dc2e166d3
adam-belahouane <adam_belahouane@outlook.com>
2021-10-20 14:41:08 +0100

    validation working

--- src/services/blogPost/index.js
+++ src/services/blogPost/index.js
@@ -4,6 +4,8 @@ import { dirname, join } from "path"
 import fs from "fs"
 import uniqid from "uniqid"
 import createHttpError from "http-errors"
+import { validationResult } from "express-validator"
+import { blogPostValidation } from "./validation.js"
 
 const blogPostsRouter = express.Router()
 
@@ -12,16 +14,21 @@ const blogPostsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "blogPos
 const getBlogPosts = () => JSON.parse(fs.readFileSync(blogPostsJSONPath))
 const writeBlogPosts = (post) => (fs.writeFileSync(blogPostsJSONPath, JSON.stringify(post)))
 
-blogPostsRouter.post("/", (req, res ,next) => {
+blogPostsRouter.post("/", blogPostValidation, (req, res ,next) => {
     try {
-        console.log(req.body)
-        const newBlogPost = { ...req.body, createdAt: new Date(), id: uniqid()}
-        console.log(newBlogPost)
-        const blogPosts = getBlogPosts()
-        blogPosts.push(newBlogPost)
-        writeBlogPosts(blogPosts)
-
-        res.status(201).send({ newBlogPost})
+        const errorsList = validationResult(req)
+        if(!errorsList.isEmpty()){
+            next(createHttpError(400, { errorsList }))
+        } else {
+            console.log(req.body)
+            const newBlogPost = { ...req.body, createdAt: new Date(), id: uniqid(), content: "HTML"}
+            console.log(newBlogPost)
+            const blogPosts = getBlogPosts()
+            blogPosts.push(newBlogPost)
+            writeBlogPosts(blogPosts)
+    
+            res.status(201).send({ newBlogPost})
+        }
     } catch (error) {
         next(error)
     }
---
+++ src/services/blogPost/validation.js
@@ -0,0 +1,8 @@
+import { body } from "express-validator"
+
+export const blogPostValidation = [
+    body("category").exists().withMessage("Category is a Required Field!"),
+    body("title").exists().withMessage("Title is a Required Field!"),
+    body("cover").exists().withMessage("Cover is a Required Field!"),
+    body("author").exists().withMessage("Author is a Required Feild!"),
+]
